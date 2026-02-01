3cd0ee0243e0a5820b2f68ce2bc31028e7242701
yuta <yuta323clan@gmail.com>
2026-02-01 19:34:59 +0900

    ALBヘルスチェック用のエンドポイントを追加

---
+++ src/main/java/com/example/hello_world/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.example.hello_world.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    
+    @GetMapping("/health")
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("OK");
+    }
+}
